bd071292f516ab92ab297fee83da12ed600fdbbf
Daria <daria.aleks1@yandex.ru>
2024-03-11 13:45:52 +0300

    TypeScript

--- src/components/ListItems/ListItems.tsx
+++ src/components/ListItems/ListItems.tsx
@@ -1,33 +1,42 @@
-import { useEffect } from "react";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
-export function ListItem (){
-
-  const [nailList, setNailList] = useState([]);
-  function list(){
-    fetch('https://makeup-api.herokuapp.com/api/v1/products.json?product_type=nail_polish')
-    .then(response => response.json())
-    .then(nailList => setNailList(nailList));
-  }
-  useEffect(() => {
-    list()
-  }, []);
-  console.log(nailList);
-  return(
-    <div>
-      {nailList?.map((card) => (
-        <div>
-         {card.product_colors.map((nail, index)=>(
-            <div key={index}>
-              {nail.hex_value}
-              <div style={{width:"100px", height:"100px", background:nail.hex_value}}>
-              </div>
-            </div>
-             ))}
-         </div>
-       ))}
-    </div>
-  )
+interface NailPolish {
+  id: string; 
+  brand: string;
+  product_colors: ProductColor[];
 }
 
+interface ProductColor {
+  hex_value: string;
+  colour_name?: string; 
+}
 
+export function ListItem() {
+  const [nailList, setNailList] = useState<NailPolish[]>([]); 
+
+  function list() {
+    fetch('https://makeup-api.herokuapp.com/api/v1/products.json?product_type=nail_polish')
+      .then(response => response.json())
+      .then((data: NailPolish[]) => setNailList(data)); 
+  }
+
+  useEffect(() => {
+    list();
+  }, []);
+
+  return (
+    <div>
+      {nailList.map((card) => (
+        <div key={card.id}> 
+          {card.product_colors.map((nail, index) => (
+            <div key={index}>
+              {nail.hex_value}
+              <div style={{ width: "100px", height: "100px", background: nail.hex_value }}>
+              </div>
+            </div>
+          ))}
+        </div>
+      ))}
+    </div>
+  );
+}
